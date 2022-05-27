ae0baf818de31afee0a04b8d8b3c5c0a020bdedb
Fauzan Aldi <mt_fauzan@metro.mail>
2022-05-27 19:07:56 +0700

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+ 
+function App() {
+  return (
+    <Router>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<ProductList />} />
+            <Route path="/add" element={<AddProduct />}/>
+            <Route path="/edit/:id" element={<EditProduct />}/>
+          </Routes>
+        </div>
+    </Router>
+  );
+}
+ 
+export default App;
